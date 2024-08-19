a7b69bb7bdd15ca8d26ec57131bea59d83f97bdf
bhubon <sdbhubon@gmail.com>
2024-08-19 21:32:04 +0600

    routes code optimized

--- public/index.php
+++ public/index.php
@@ -14,18 +14,15 @@ $request = $_SERVER['REQUEST_URI'];
 
 $request = explode("?", $request)[0];
 
+[$controller, $action] = $routes[$request] ?? [null, null];
 
-if (isset($routes[$request])) {
 
-    $controller = $routes[$request][0] ?? null;
-    $action = $routes[$request][1] ?? null;
+if ($controller && $action) {
+    
+    $newController = new $controller();
 
-    if ($controller && $action) {
-        $newController = new $controller();
+    $newController->$action();
 
-        $newController->$action();
-    }
-
-}else{
+} else {
     throw new Exception("404 Not Found");
 }
